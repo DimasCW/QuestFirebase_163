533606a6ee896f5305027e1c06572e69d1df5855
DimasCW <cdimas292@gmail.com>
2025-01-11 09:02:09 +0700

    membuat data class MahasiswaEvent untuk tempat menyimpan data input dari form

--- app/src/main/java/com/example/kotlin_firebase/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/kotlin_firebase/ui/ViewModel/InsertViewModel.kt
@@ -1,11 +1,22 @@
 package com.example.kotlin_firebase.ui.ViewModel
 
 import androidx.lifecycle.ViewModel
+import com.example.kotlin_firebase.model.Mahasiswa
 import com.example.kotlin_firebase.repository.MahasiswaRepository
 
 class InsertViewModel(
     private val mhs: MahasiswaRepository
 ) : ViewModel() {
+    //data class variable yang menyimpan data input form
+    data class MahasiswaEvent(
+        val nim: String = "",
+        val nama: String = "",
+        val jenisKelamin: String = "",
+        val alamat: String = "",
+        val kelas: String = "",
+        val angkatan: String = ""
+    )
+
     //Menyimpan input form ke dalam entity
     fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa(
         nim = nim,
